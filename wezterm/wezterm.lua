ebeacb674e87e5c6716bae6ee42d92d5cd5ba2bb
kkyouma <144501527+kkyouma@users.noreply.github.com>
2025-01-22 16:15:00 -0300

    add killword keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,12 +12,32 @@ config.window_background_opacity = 1
 config.max_fps = 144
 config.font = wezterm.font("JetBrains Mono")
 config.window_padding = {
-	left = 2,
-	right = 2,
-	top = 0,
-	bottom = 0,
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
 }
 config.underline_thickness = 1.0
 config.underline_position = -2.0
+config.default_cursor_style = "SteadyBar"
+
+config.keys = {
+  {
+    key = "Backspace",
+    mods = "CTRL",
+    action = wezterm.action.SendKey({
+      key = "w",
+      mods = "CTRL",
+    }),
+  },
+  {
+    key = "Delete",
+    mods = "CTRL",
+    action = wezterm.action.SendKey({
+      key = "Delete",
+      mods = "CTRL",
+    }),
+  },
+}
 
 return config
