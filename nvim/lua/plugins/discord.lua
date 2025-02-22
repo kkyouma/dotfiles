3847587414da89181f1ad46a2527fe545b290954
kkyouma <144501527+kkyouma@users.noreply.github.com>
2025-02-22 20:52:20 -0300

    Update nvim icon for discord

--- nvim/lua/plugins/discord.lua
+++ nvim/lua/plugins/discord.lua
@@ -1,7 +1,9 @@
 return {
-	"IogaMaster/neocord",
-	event = "VeryLazy",
-	config = function()
-		require("neocord").setup()
-	end,
+  'IogaMaster/neocord',
+  event = 'VeryLazy',
+  config = function()
+    require('neocord').setup {
+      logo = 'https://yoolk.ninja/wp-content/uploads/2020/11/Apps-Neovim-1024x1024.png',
+    }
+  end,
 }
