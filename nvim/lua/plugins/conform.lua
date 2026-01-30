fd998f3275ccaa02515fc318efbb2c6978369ec2
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-01-30 20:27:16 -0300

    Add sql linter and formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -17,15 +17,12 @@ return {
   opts = {
     notify_on_error = true,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
         return {
-          timeout_ms = 500,
+          timeout_ms = 1000,
           lsp_format = 'fallback',
         }
       end
@@ -37,13 +34,11 @@ return {
       typescript = { 'prettier' },
       css = { 'prettier' },
       html = { 'prettier' },
-      astro = { 'astro-language-server astro' },
+      astro = { 'prettier' },
       yaml = { 'prettier' },
       markdown = { 'prettier' },
       json = { 'prettier' },
-      sql = { 'sql_formatter' },
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      sql = { 'sqlfluff' },
     },
   },
 }
