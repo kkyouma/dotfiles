62810ee66dd975ad61cd7e101e6380618df9f00c
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-01-30 20:25:15 -0300

    Add new color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,50 +1,52 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "tokyonight_night"
+-- Theme
+config.color_scheme = "deep_blue_custom"
 
+-- Wayland + Blur
+config.enable_wayland = true
+config.window_background_opacity = 0.85
+
+-- Terminal
 config.term = "wezterm"
-config.front_end = "OpenGL"
 config.custom_block_glyphs = true
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
-config.window_background_opacity = 1
 config.max_fps = 144
+
+-- Font
 config.font = wezterm.font("JetBrains Mono")
+
+-- Layout
 config.window_padding = {
 	left = 2,
 	right = 2,
 	top = 0,
 	bottom = 0,
 }
+
+-- Cursor & underline
 config.underline_thickness = 1.0
 config.underline_position = -2.0
 config.default_cursor_style = "SteadyBar"
+
+-- Startup
 config.initial_rows = 27
 config.initial_cols = 100
 config.window_close_confirmation = "NeverPrompt"
 
+-- Keys
 config.keys = {
-	-- {
-	-- 	key = "Tab",
-	-- 	mods = "CTRL",
-	-- 	action = wezterm.action.DisableDefaultAssignment,
-	-- },
 	{
 		key = "Backspace",
 		mods = "CTRL",
-		action = wezterm.action.SendKey({
-			key = "w",
-			mods = "CTRL",
-		}),
+		action = wezterm.action.SendKey({ key = "w", mods = "CTRL" }),
 	},
 	{
 		key = "Delete",
 		mods = "CTRL",
-		action = wezterm.action.SendKey({
-			key = "Delete",
-			mods = "CTRL",
-		}),
+		action = wezterm.action.SendKey({ key = "Delete", mods = "CTRL" }),
 	},
 }
 
