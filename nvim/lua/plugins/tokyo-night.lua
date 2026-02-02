e29b552064bd7be6bf53f3719bc7047e68a7d531
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-02-02 13:14:19 -0300

    Add minor color correction

--- nvim/lua/plugins/tokyo-night.lua
+++ nvim/lua/plugins/tokyo-night.lua
@@ -6,12 +6,12 @@ return {
 
     local bg = '#011628'
     local bg_dark = '#011423'
-    local bg_highlight = '#143652'
+    local bg_highlight = '#03223A'
     local bg_search = '#0A64AC'
     local bg_visual = '#275378'
     local fg = '#CBE0F0'
     local fg_dark = '#B4D0E9'
-    local fg_gutter = '#627E97'
+    local fg_gutter = '#102d45'
     local border = '#547998'
 
     require('tokyonight').setup {
