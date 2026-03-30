9c0ed5e9aef4e7d02da72e8745c75230d32b3cf2
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-03-30 05:39:17 -0300

    fix: remove duplicated autoatach and fix go to definition between buffers

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -8,6 +8,9 @@ return {
     'b0o/schemastore.nvim',
   },
   config = function()
+    -------------------------
+    -- AUTO CMD --
+    ------------------------
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -15,9 +18,22 @@ return {
           mode = mode or 'n'
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
+        -- Native
         map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
         map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
         map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        map('<K>', vim.lsp.buf.hover, 'Hover Documentation')
+
+        -- Telescope
+        local builtin = require 'telescope.builtin'
+
+        map('grd', builtin.lsp_definitions, '[G]oto [D]efinition')
+        map('grr', builtin.lsp_references, '[G]oto [R]eferences')
+        map('gri', builtin.lsp_implementations, '[G]oto [I]mplementation')
+        map('grt', builtin.lsp_type_definitions, '[G]oto [T]ype Definition')
+        map('gO', builtin.lsp_document_symbols, 'Open Document Symbols')
+        map('gW', builtin.lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client:supports_method('textDocument/documentHighlight', event.buf) then
@@ -48,6 +64,10 @@ return {
         end
       end,
     })
+
+    -------------------------
+    -- LSP CONFIGURATIONS --
+    ------------------------
 
     local capabilities = require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -181,8 +201,8 @@ return {
     }
 
     -- Priority language servers (first to start)
-    vim.lsp.config('vtsls', servers.vtsls)
-    vim.lsp.enable 'vtsls'
+    -- vim.lsp.config('vtsls', servers.vtsls)
+    -- vim.lsp.enable 'vtsls'
 
     -- Habilitar todos los servidores del loop
     for name, server in pairs(servers) do
